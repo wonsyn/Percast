86f02351a5a78c40a6366a6dfb0b24caa19aaa7d
hsh102103 <hsh102103@naver.com>
2022-09-16 19:38:06 +0900

    Hwang_refactor: notice 관련 refactoring
    dto를 response, request 단위로 쪼갬
    controller 타입 변경(ResponseEntity -> Custom DTO)
    사용자 정의 Exception 추가

---
+++ BE/percast/src/main/java/b101/percast/Exception/noticeException/NoticeNotFoundException.java
@@ -0,0 +1,4 @@
+package b101.percast.Exception.noticeException;
+
+public class NoticeNotFoundException extends RuntimeException {
+}
---
+++ BE/percast/src/main/java/b101/percast/dto/notice/NoticeFindAllResponseDto.java
@@ -0,0 +1,32 @@
+package b101.percast.dto.notice;
+
+import b101.percast.entity.Notice;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NoticeFindAllResponseDto {
+    private Long id;
+
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    private LocalDateTime date;
+    private String title;
+    private String content;
+
+    /* Entity -> DTO */
+    public NoticeFindAllResponseDto(Notice entity) {
+        this.id = entity.getId();
+        this.date = entity.getDate();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+    }
+
+}
