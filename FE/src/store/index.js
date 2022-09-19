d4fa640ed33b1d0bfe8a41d2ba587ee6a2bc3e10
kyojun <kyojun2002@naver.com>
2022-09-19 17:55:34 +0900

    feat: S07P22B101-87 계절별 질병정보 입력중

--- FE/src/store/index.js
+++ FE/src/store/index.js
@@ -2,6 +2,7 @@ import { createStore } from 'vuex'
 
 import createdPersistedState from "vuex-persistedstate";
 
+import aboutStore from '@/store/modules/aboutStore.js';
 import chatStore from '@/store/modules/chatStore.js';
 import navStore from '@/store/modules/navStore.js';
 import menuStore from '@/store/modules/menuStore.js';
@@ -10,6 +11,7 @@ import mapStore from '@/store/modules/mapStore.js';
 
 export default createStore({
   modules: {
+    aboutStore,
     chatStore,
     navStore,
     menuStore,
