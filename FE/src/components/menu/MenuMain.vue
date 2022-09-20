669d479c7b892000f56711f3379d3d3af72f84c4
jeongkyojun <kyojun2002@naver.com>
2022-09-20 16:15:52 +0900

    feat: S07P22B101-85 지도이미지 결합

--- FE/src/components/menu/MenuMain.vue
+++ FE/src/components/menu/MenuMain.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
+    <div id="album">
+      <div id="bg_map">
+        <div id="bubble" class="seoul">
+          <div id="pic">
+
+          </div>
+        </div>
+      </div>
+    </div>
     <ul v-if="depth==0">
       <menu-main-item v-for="(items,index) in sido" :key="items.index=index" v-bind="items"></menu-main-item>
     </ul>
     <ul v-else-if="depth==1">
       <menu-main-item v-for="(items,index) in gugun" :key="items.index=index" v-bind="items"></menu-main-item>
-
       <li @click="back()">
         뒤로가기
       </li>
@@ -38,5 +46,22 @@ export default {
 </script>
 
 <style>
+#bg_map {
+  position: relative;
+  width: 100px;
+  height: 100px;
+  z-index: 1;
+  background-image: url("../../assets/map.png");
+  background-size: cover;
+}
 
+#pic {
+  position: relative;
+  width: 50px;
+  height: 50px;
+  z-index: 2;
+  color: red;
+  background-size: cover;
+  background-image: url("../../assets/bubble.png");
+}
 </style>
