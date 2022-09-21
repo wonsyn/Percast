66782ead925fc16d8e36af0d3902b4bded2ba841
wonsyn <sde3715@naver.com>
2022-09-21 11:12:39 +0900

    wonsyn_refactor: S07P22B101-164 Input 테이블 수정

--- BE/percast/src/main/java/b101/percast/entity/Input.java
+++ BE/percast/src/main/java/b101/percast/entity/Input.java
@@ -1,35 +1,30 @@
 package b101.percast.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.io.Serializable;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-public class Input implements Serializable {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long inputNum;
-
+@Builder
+public class Input {
     @Id
-    @OneToOne
-    @JoinColumn(name = "sido_Code", nullable = false)
-    private Sido sido;
-
+    @Column(name = "sido_code")
+    private Long sidoCode;
     private double so2;    // 아황산가스
     private double co;     // 일산화탄소
     private double o3;     // 오존
     private double no2;    // 이산화질소
     private double pm10;   // 미세먼지
-    private int tempAvg;    // 1시간 기온
-    private int tempMin;    // 최저 온도
-    private int tempMax;    // 최고 온도
+    private double tempAvg;    // 1시간 기온
+    private double tempMin;    // 최저 온도
+    private double tempMax;    // 최고 온도
     private double windAvg;    // 풍속
-    private int humid;  // 습도
-    private int rain;   // 강수량 (강수없음)
+    private double humid;  // 습도
+    private double rain;   // 강수량 (강수없음 = 0)
 }
