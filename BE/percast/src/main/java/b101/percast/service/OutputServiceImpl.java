e78f6df5bc6e9d0281715a22df4765644859b59d
Suhwan2 <hsh102103@naver.com>
2022-09-22 18:17:01 +0900

    Hwang_Feat: S07P22B101-108 output 테이블에 식중독 위험지수 업데이트

--- BE/percast/src/main/java/b101/percast/service/OutputServiceImpl.java
+++ BE/percast/src/main/java/b101/percast/service/OutputServiceImpl.java
@@ -1,16 +1,18 @@
 package b101.percast.service;
 
 import b101.percast.dto.output.OutputFindAllResponseDto;
+import b101.percast.entity.Output;
 import b101.percast.repository.OutputRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class OutputServiceImpl implements OutputService{
+public class OutputServiceImpl implements OutputService {
 
     private final OutputRepository outputRepository;
 
@@ -18,5 +20,14 @@ public class OutputServiceImpl implements OutputService{
     public List<OutputFindAllResponseDto> findAll() {
         return outputRepository.findAll().stream().map(OutputFindAllResponseDto::new)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public void save(List<String> list) {
+        List<Output> outputList = outputRepository.findAll();
+        for (int i = 0; i < list.size(); i++) {
+            Output output = outputList.get(i);
+            output.updateFoodPoison(Double.parseDouble(list.get(i)));
+        }
     }
 }
