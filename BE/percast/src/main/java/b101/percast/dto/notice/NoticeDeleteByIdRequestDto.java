d6ec4eb9e383f2f7c8ba245166e5d2673e051afa
hsh102103 <hsh102103@naver.com>
2022-09-23 15:56:46 +0900

    Hwang_Feat: S07P22B101-179 NoticeDeleteByIdRequestDto 누락된 swagger 추가
    멤버변수 id에 @ApiModelProperty 추가

--- BE/percast/src/main/java/b101/percast/dto/notice/NoticeDeleteByIdRequestDto.java
+++ BE/percast/src/main/java/b101/percast/dto/notice/NoticeDeleteByIdRequestDto.java
@@ -2,6 +2,7 @@ package b101.percast.dto.notice;
 
 import b101.percast.entity.Notice;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +14,8 @@ import lombok.NoArgsConstructor;
 @Data
 @ApiModel(value = "공지사항 삭제 정보", description = "공지사항 번호를 통한 글삭제 요청에 사용되는 DTO입니다.")
 public class NoticeDeleteByIdRequestDto {
+
+    @ApiModelProperty(value = "글 번호")
     private Long id;
 
     /* DTO -> Entity */
