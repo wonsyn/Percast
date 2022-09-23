c0e2f6c0f65ae85e28548c23b33c24fb4e6e9731
wonsyn <sde3715@naver.com>
2022-09-23 15:41:16 +0900

    wonsyn_feat: S07P22B101-157 Swagger 작성 및 불필요 멤버변수 삭제

--- BE/percast/src/main/java/b101/percast/dto/Qna/QnaSaveRequest.java
+++ BE/percast/src/main/java/b101/percast/dto/Qna/QnaSaveRequest.java
@@ -1,7 +1,8 @@
 package b101.percast.dto.Qna;
 
-import b101.percast.entity.Answer;
 import b101.percast.entity.Qna;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,16 +14,18 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ApiModel(value = "QnA 작성 객체", description = "작성할 QnA의 정보가 담겨있는 객체")
 public class QnaSaveRequest {
+    @ApiModelProperty(value = "QnA 제목")
     private String title;
+    @ApiModelProperty(value = "QnA 내용")
     private String content;
+    @ApiModelProperty(value = "QnA 비밀번호")
     private String password;
-    private String tel;
-    private Answer answer;
 
     public Qna toEntity() {
         return Qna.builder()
                 .title(title).content(content).date(LocalDateTime.now())
-                .password(password).tel(tel).answer(answer).build();
+                .password(password).answer(null).build();
     }
 }
--- BE/percast/src/main/java/b101/percast/dto/Qna/QnaUpdateRequest.java
+++ BE/percast/src/main/java/b101/percast/dto/Qna/QnaUpdateRequest.java
@@ -1,5 +1,7 @@
 package b101.percast.dto.Qna;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +12,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ApiModel(value = "QnA 수정 객체", description = "수정될 QnA 정보를 담고 있는 객체")
 public class QnaUpdateRequest {
+        @ApiModelProperty(value = "수정할 QnA ID")
         private Long id;
+        @ApiModelProperty(value = "수정할 QnA 제목")
         private String title;
+        @ApiModelProperty(value = "수정할 QnA 내용")
         private String content;
-        private String tel;
 }
