b6f9784ca7bdaaabb51bdf040adfab667540534a
hsh102103 <hsh102103@naver.com>
2022-09-23 14:29:53 +0900

    Hwang_Feat: S07P22B101-179 Admin관련 Swagger 추가
    Admin관련 dto, controller에 Swagger추가

--- BE/percast/src/main/java/b101/percast/controller/AdminController.java
+++ BE/percast/src/main/java/b101/percast/controller/AdminController.java
@@ -3,6 +3,9 @@ package b101.percast.controller;
 import b101.percast.config.PasswordEncrypt;
 import b101.percast.dto.admin.AdminSaveDto;
 import b101.percast.service.AdminService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,14 +26,21 @@ public class AdminController {
 
     // 처음에 패스워드 입력받고 난수 생성해서 응답에 보내주는 API + DB에 난수, pass저장되야됨
     @PostMapping
-    public ResponseEntity<Map<String, Object>> regist(@RequestBody AdminSaveDto dto){
+    @ApiOperation(value = "어드민계정 등록", notes = "요청의 패스워드 확인해서 암호화된 패스워드를 db업데이트 후 반환한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "등록 성공"),
+            @ApiResponse(code = 400, message = "데이터를 찾을 수 없음. 또는 잘못된 요청"),
+            @ApiResponse(code = 401, message = "비밀번호가 틀려서 접근할 수 없습니다."),
+            @ApiResponse(code = 500, message = "서버 에러")
+    })
+    public ResponseEntity<Map<String, Object>> regist(@RequestBody AdminSaveDto dto) {
         Map<String, Object> map = new HashMap<>();
         // 비밀번호 체크
-        if(!adminService.checkByPass(dto.getPassword())){
-            map.put("message","fail");
+        if (!adminService.checkByPass(dto.getPassword())) {
+            map.put("message", "fail");
             return ResponseEntity.status(401).body(map);
         }
-        map.put("message","success");
+        map.put("message", "success");
         String encryptedPassword = PasswordEncrypt.encrypt(dto.getPassword());
         System.out.println(encryptedPassword);
         adminService.update(dto.getPassword(), encryptedPassword);
