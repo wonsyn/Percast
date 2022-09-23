12971286f9388e834619b09f8ee8b108dd09e762
wonsyn <sde3715@naver.com>
2022-09-23 15:39:56 +0900

    wonsyn_feat: S07P22B101-157 Answer Entity Swagger 추가

--- BE/percast/src/main/java/b101/percast/entity/Answer.java
+++ BE/percast/src/main/java/b101/percast/entity/Answer.java
@@ -1,6 +1,8 @@
 package b101.percast.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,17 +17,21 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Getter
 @Builder
+@ApiModel(value = "Answer Entity", description = "Answer의 정보를 담고 있는 Entity")
 public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ans_id")
+    @ApiModelProperty(value = "Answer ID")
     private Long id;
 
     @Column(name = "ans_content", nullable = false)
+    @ApiModelProperty(value = "Answer 내용")
     private String content;
     @Column(name = "ans_date", nullable = false)
     @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     @CreatedDate
+    @ApiModelProperty(value = "Answer 작성일자")
     private LocalDateTime date;
 
     public Answer update(String content) {
