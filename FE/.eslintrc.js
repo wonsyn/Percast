5c968496a43a9b64ec2f4b38c7f052561bd37731
Suhwan2 <hsh102103@naver.com>
2022-09-26 14:54:15 +0900

    Hwang_Style:  프리티어 적용

--- FE/.eslintrc.js
+++ FE/.eslintrc.js
@@ -3,15 +3,12 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/vue3-essential"
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/vue3-essential', 'plugin:prettier/recommended'],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
 };
