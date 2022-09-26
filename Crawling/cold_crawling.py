0fb490afe009f6674433eb98343332a6460702f5
DESKTOP-KVCQHCD\SSAFY <hany961031@naver.com>
2022-09-26 16:29:58 +0900

    han Feat: S07P22B101-187 EC2 크롤링 구현

---
+++ Crawling/cold_crawling.py
@@ -0,0 +1,41 @@
+import tweepy
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+import mecab_ko as MeCab
+from datetime import date, timedelta
+from sqlalchemy import create_engine
+import pymysql
+
+db_connection_str = 'mysql+pymysql://percast:ssafy7@3.34.43.90:3306/percast?charset=utf8'
+db_connection = create_engine(db_connection_str, encoding='utf-8')
+conn = db_connection.connect()
+
+today = date.today()
+yesterday = date.today()
+
+tagger = MeCab.Tagger()
+# 발급받은 키 입력
+twitter_consumer_key = "7JUcMFJpKejaAy4VRwB4MHaIh"
+twitter_consumer_secret = "FAOg16Rz3WWxvqrdFkZbaWgcAyGaGB1s4BWEgYWY1IoBfEhaR1"
+twitter_access_token = "1569900011550568448-blsCGbNKpKMYgj6uLhoL8o99SycQ1z"
+twitter_access_secret = "NzAkQn2B7fLxXiAf5hYnWnhI4XmSBv28W0ukHxRu0I1Tv"
+
+auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
+auth.set_access_token(twitter_access_token, twitter_access_secret)
+api = tweepy.API(auth)
+
+tweets = []
+# limit = 100
+str = "감기 since:" + yesterday.strftime('%Y-%m-%d')
+
+query = str
+if query != 0:
+  for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+    tmp = tagger.parse(tweet.content).split()
+    for i in range(1, len(tmp), 2):
+      ttmp = tmp[i].split(",")
+      if ttmp[0].startswith('NNG') or ttmp[0].startswith('NNP'):
+        tweets.append(ttmp[3])
+df = pd.DataFrame(tweets, columns=['word'])
+df.to_csv('cold_today.csv')
+df.to_sql(name='cold_input', con=db_connection, if_exists='replace', index=False)
