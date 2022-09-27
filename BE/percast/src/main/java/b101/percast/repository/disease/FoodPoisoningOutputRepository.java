492fb52f4dea395d3bab1d08cea87952be0a5177
hsh102103 <hsh102103@naver.com>
2022-09-27 14:51:12 +0900

    Hwang_Feat: S07P22B101-261 Twit 크롤링결과별 Repo생성

---
+++ BE/percast/src/main/java/b101/percast/repository/disease/FoodPoisoningOutputRepository.java
@@ -0,0 +1,7 @@
+package b101.percast.repository.disease;
+
+import b101.percast.entity.disease.AsthmaOutput;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodPoisoningOutputRepository extends JpaRepository<AsthmaOutput, String> {
+}
