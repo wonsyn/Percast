5887ba81670333e454eef5b562582ca7fb4ead24
Suhwan2 <hsh102103@naver.com>
2022-09-28 11:03:20 +0900

    Hwang_Feat: S07P22B101-236 Twit관련 Swagger 추가

--- BE/percast/src/main/java/b101/percast/dto/twit/TwitFindAllResponseDto.java
+++ BE/percast/src/main/java/b101/percast/dto/twit/TwitFindAllResponseDto.java
@@ -2,6 +2,8 @@ package b101.percast.dto.twit;
 
 import b101.percast.entity.Notice;
 import b101.percast.entity.Twit;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +13,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@ApiModel(value = "Twit 크롤링 결과 리스트"
+        , description = "Twit 크롤링 결과 리스트를 담고있는 객체")
 public class TwitFindAllResponseDto {
+    
+    @ApiModelProperty(value = "Twit ID")
     private Long id;
+
+    @ApiModelProperty(value = "Twit 질병명")
     private String disease;
+
+    @ApiModelProperty(value = "Twit 횟수")
     private Long count;
+
+    @ApiModelProperty(value = "Twit 키워드")
     private String keyword;
 
     /* Entity -> DTO */
