6479bcdb1d20a66207d660aa387c8e82e91ed491
wonsyn <sde3715@naver.com>
2022-09-29 10:04:09 +0900

    wonsyn_feat: S07P22B101-235 공지사항 게시판 게시글 작성창 구현

--- FE/src/components/board/Notice/NoticeWrite.vue
+++ FE/src/components/board/Notice/NoticeWrite.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+    <div class="percast-font my-5" style="font-size: 50px; font-weight: bolder">
+      공지사항
+    </div>
     <form v-on:submit="writeNotice" class="mt-5" ref="form">
       <div class="my-3 d-flex">
         <label class="me-3 percast-font" for="title">제목</label>
@@ -8,7 +11,8 @@
           placeholder="제목을 입력해 주세요"
           name="title"
           v-model="title"
-          style="width: 90%"
+          style="width: 95%"
+          required
         />
       </div>
       <div class="my-3 d-flex">
@@ -17,15 +21,19 @@
           name="content"
           placeholder="내용을 입력해 주세요"
           v-model="content"
-          rows="3"
-          max-rows="6"
-          style="width: 90%"
+          rows="10"
+          max-rows="20"
+          style="width: 95%"
+          required
         ></b-form-textarea>
       </div>
-      <div class="d-flex justify-content-end pe-3 mb-6">
-        <button type="submit" class="btn btn-success d-flex justify-end">
-          등록
-        </button>
+      <div class="row">
+        <div class="col d-flex my-3">
+          <button class="btn btn-info" @click="moveToList">뒤로가기</button>
+        </div>
+        <div class="col d-flex justify-content-end my-3">
+          <button type="submit" class="btn btn-success">등록</button>
+        </div>
       </div>
     </form>
   </div>
@@ -41,6 +49,11 @@ export default {
     };
   },
   methods: {
+    moveToList() {
+      this.$router.push({
+        name: "NoticeList",
+      });
+    },
     writeNotice() {
       const newNotice = {
         title: this.title,
