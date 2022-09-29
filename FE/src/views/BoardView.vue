1f7be2cb7d745522486250127b8572e1c7f4e8a3
wonsyn <sde3715@naver.com>
2022-09-29 18:03:15 +0900

    wonsyn_feat: S07P22B101-232 Board 타입에 따른 버튼 색깔 변경 구현

--- FE/src/views/BoardView.vue
+++ FE/src/views/BoardView.vue
@@ -1,13 +1,46 @@
 <template>
-  <router-link to="/board/notice/list">공지사항</router-link>
-  <div>|</div>
-  <router-link to="/board/qna/list">QNA</router-link>
+  <div class="d-flex justify-content-center mt-5">
+    <router-link
+      id="notice-menu"
+      class="btn btn-success"
+      to="/board/notice/list"
+      @click="changeNotice"
+      >공지사항</router-link
+    >
+    <div>&nbsp;|&nbsp;</div>
+    <router-link
+      id="qna-menu"
+      class="btn btn-info"
+      to="/board/qna/list"
+      @click="changeQna"
+      >QNA</router-link
+    >
+  </div>
   <div></div>
   <router-view></router-view>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    changeNotice() {
+      const noticeBtn = document.getElementById("notice-menu");
+      const qnaBtn = document.getElementById("qna-menu");
+      noticeBtn.classList.remove("btn-info");
+      qnaBtn.classList.remove("btn-success");
+      noticeBtn.classList.add("btn-success");
+      qnaBtn.classList.add("btn-info");
+    },
+    changeQna() {
+      const noticeBtn = document.getElementById("notice-menu");
+      const qnaBtn = document.getElementById("qna-menu");
+      qnaBtn.classList.remove("btn-info");
+      noticeBtn.classList.remove("btn-success");
+      qnaBtn.classList.add("btn-success");
+      noticeBtn.classList.add("btn-info");
+    },
+  },
+};
 </script>
 
 <style scoped></style>
