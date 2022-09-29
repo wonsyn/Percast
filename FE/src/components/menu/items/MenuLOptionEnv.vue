e635e36005d1ff6256035e2a93dbfb950a9fd2bc
DESKTOP-KVCQHCD\SSAFY <hany961031@naver.com>
2022-09-29 18:00:22 +0900

    han style: 메뉴 왼쪽 메뉴

--- FE/src/components/menu/items/MenuLOptionEnv.vue
+++ FE/src/components/menu/items/MenuLOptionEnv.vue
@@ -1,39 +1,28 @@
 <template>
   <b-card id="menu_card">
-    <b-card-body>
-      <b-row>
-        <b-col cols="4">
-          <b-card
-            class="inner_card"
-            style="background-color: #1a2844; color: white"
-          >
-            <div id="dust"></div>
-            <div>미세먼지</div>
-            <b-card-text>{{ environment.pm10 }}</b-card-text>
-          </b-card>
-        </b-col>
-        <b-col cols="4">
-          <b-card
-            class="inner_card"
-            style="background-color: #1a2844; color: white"
-          >
-            <div id="super_dust"></div>
-            <div>일산화탄소</div>
-            <b-card-text>{{ environment.co }}</b-card-text>
-          </b-card>
-        </b-col>
-        <b-col cols="4">
-          <b-card
-            class="inner_card"
-            style="background-color: #1a2844; color: white"
-          >
-            <div id="ozon"></div>
-            <div>오존</div>
-            <b-card-text>{{ environment.o3 }}</b-card-text>
-          </b-card>
-        </b-col>
-      </b-row>
-    </b-card-body>
+    <b-row>
+      <b-col class="cards" cols="4">
+        <div class="picto">
+          <img :src="require(`@/assets/dust.svg`)" />
+        </div>
+        <div>미세먼지</div>
+        <b-card-text>{{ environment.pm10 }}</b-card-text>
+      </b-col>
+      <b-col class="cards" cols="4">
+        <div class="picto">
+          <img :src="require(`@/assets/super_dust.svg`)" />
+        </div>
+        <div>일산화탄소</div>
+        <b-card-text>{{ environment.co }}</b-card-text>
+      </b-col>
+      <b-col class="cards" cols="4">
+        <div class="picto">
+          <img :src="require(`@/assets/ozon.svg`)" />
+        </div>
+        <div>오존</div>
+        <b-card-text>{{ environment.o3 }}</b-card-text>
+      </b-col>
+    </b-row>
   </b-card>
 </template>
 
@@ -53,4 +42,13 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.cards {
+  justify-content: center;
+  color: white;
+}
+
+.picto {
+  margin: 10px 0px;
+}
+</style>
