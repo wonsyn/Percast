b1861608f46f08d934cad65f1172e21527dc4ac5
jeongkyojun <kyojun2002@naver.com>
2022-09-30 16:44:42 +0900

    마커 이미지 변경

--- FE/src/components/maps/MapsMain.vue
+++ FE/src/components/maps/MapsMain.vue
@@ -280,8 +280,9 @@ export default {
     // 수정본
     addMarker(position, order, num) {
       if (this.currNum == -1) return;
-      var imageSrc =
-        "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_category.png"; // 마커 이미지 url, 스프라이트 이미지를 씁니다
+      var imageSrc = "https://ifh.cc/g/v5F5T5.png";
+      // 유효기간 : 2023.06.07
+      //"https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_category.png"; // 마커 이미지 url, 스프라이트 이미지를 씁니다
       const imageSize = new kakao.maps.Size(27, 28); // 마커 이미지의 크기
       const imgOptions = {
         spriteSize: new kakao.maps.Size(72, 208), // 스프라이트 이미지의 크기
@@ -473,8 +474,7 @@ body {
 }
 
 #category li .category_bg {
-  background: url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_category.png)
-    no-repeat;
+  background: url(../../assets/marker.png) no-repeat;
 }
 
 #category li .hospital {
