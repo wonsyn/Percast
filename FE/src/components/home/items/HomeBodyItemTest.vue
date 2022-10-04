4a33ed453b87a391ae771d2c79137f8ebd247568
DESKTOP-KVCQHCD\SSAFY <jangwonseok17@gmail.com>
2022-10-04 16:11:13 +0900

    Jang_fix: AOS CSS 수정

--- FE/src/components/home/items/HomeBodyItemTest.vue
+++ FE/src/components/home/items/HomeBodyItemTest.vue
@@ -5,17 +5,23 @@
       class="justify-content-md-center my-5"
       style="color: #5673eb"
     >
-      <b-col data-aos="fade-right" data-aos-offset="300">
+      <b-col data-aos="fade-right" data-aos-offset="100">
         <div>
           <div>
-            <h1>{{ title }}</h1>
+            <div id="welcome_title">{{ title }}</div>
           </div>
-          <div>
-            {{ comments }}
+          <div id="welcom_comments_left" class="mt-5">
+            {{ comments1 }}
+          </div>
+          <div id="welcom_comments_left">
+            {{ comments2 }}
+          </div>
+          <div id="welcom_comments_left">
+            {{ comments3 }}
           </div>
         </div>
       </b-col>
-      <b-col data-aos="fade-left" data-aos-offset="300" class="mx-3">
+      <b-col data-aos="fade-left" data-aos-offset="100" class="mx-3">
         <div>
           <img :src="require(`@/assets/${imageUrl}`)" />
         </div>
@@ -32,7 +38,7 @@
       </b-col>
     </b-row>
     <b-row v-else class="justify-content-md-center my-5" style="color: white">
-      <b-col data-aos="fade-right" data-aos-offset="300" class="mx-3">
+      <b-col data-aos="fade-right" data-aos-offset="100" class="mx-3">
         <div>
           <img :src="require(`@/assets/${imageUrl}`)" />
         </div>
@@ -47,13 +53,19 @@
           </b-button>
         </div>
       </b-col>
-      <b-col data-aos="fade-left" data-aos-offset="300">
+      <b-col data-aos="fade-left" data-aos-offset="100">
         <div>
           <div>
-            <h1>{{ title }}</h1>
+            <div id="welcome_title">{{ title }}</div>
           </div>
-          <div>
-            {{ comments }}
+          <div id="welcom_comments_right" class="mt-5">
+            {{ comments1 }}
+          </div>
+          <div id="welcom_comments_right">
+            {{ comments2 }}
+          </div>
+          <div id="welcom_comments_right">
+            {{ comments3 }}
           </div>
         </div>
       </b-col>
@@ -70,7 +82,9 @@ export default {
   props: {
     index: Number,
     title: String,
-    comments: String,
+    comments1: String,
+    comments2: String,
+    comments3: String,
     menu: String,
     name: String,
     imageUrl: String,
@@ -94,6 +108,7 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap");
 #menu {
   margin: 10px;
   width: 99%;
@@ -117,5 +132,25 @@ img {
   height: "auto";
   max-width: 600px;
   max-height: 600px;
+}
+#welcome_title {
+  font-size: 50px;
+  font-weight: 1000;
+  font-family: "Nanum Gothic";
+}
+/* 위 우 아래 좌 */
+#welcom_comments_right {
+  font-size: 20px;
+  font-weight: 600;
+  font-family: "Nanum Gothic";
+  text-align: center;
+  /* margin: 10px 0px 10px 150px; */
+}
+#welcom_comments_left {
+  font-size: 20px;
+  font-weight: 600;
+  font-family: "Nanum Gothic";
+  text-align: center;
+  /* margin: 10px 0px 10px 150px; */
 }
 </style>
